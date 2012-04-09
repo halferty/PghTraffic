e4a04337cbfc82d9dd8098ca962fc551acb2ce08
Ed Halferty <edhalferty@gmail.com>
2012-04-09 09:14:05 -0400

    Fixed build.

---
+++ PghTraffic/src/com/ehalferty/pghtraffic/CamerasFavoritesActivity.java
@@ -0,0 +1,39 @@
+package com.ehalferty.pghtraffic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ehalferty.pghtraffic.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Pair;
+
+public class CamerasFavoritesActivity extends Activity {
+	
+	private List<Pair<String, String>> name_and_id = new ArrayList<Pair<String, String>>();
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cameras_favorites_layout);
+        try {
+			LoadCameraInfo("ids_to_names.csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+	
+	private void LoadCameraInfo(String fileName) throws IOException {
+		BufferedReader inFile = new BufferedReader(new FileReader(fileName));
+		String line;
+		while ((line = inFile.readLine()) != null) {
+			String[] splitLine = line.split(",");
+			name_and_id.add(new Pair<String, String>(splitLine[1], splitLine[0]));
+		}
+	}
+	
+}
---
+++ PghTraffic/src/com/ehalferty/pghtraffic/CamerasMapActivity.java
@@ -0,0 +1,40 @@
+package com.ehalferty.pghtraffic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ehalferty.pghtraffic.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Pair;
+
+public class CamerasMapActivity extends Activity {
+	
+	private List<Pair<String, Point2D>> id_and_coords = new ArrayList<Pair<String, Point2D>>();
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cameras_map_layout);
+        
+        try {
+			LoadCameraInfo("coords_to_ids.csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+	
+	private void LoadCameraInfo(String fileName) throws IOException {
+		BufferedReader inFile = new BufferedReader(new FileReader(fileName));
+		String line;
+		while ((line = inFile.readLine()) != null) {
+			String[] splitLine = line.split(",");
+			Point2D pt = new Point2D(Float.parseFloat(splitLine[0]), Float.parseFloat(splitLine[1]));
+			id_and_coords.add(new Pair<String, Point2D>(splitLine[2], pt));
+		}
+	}
+}
